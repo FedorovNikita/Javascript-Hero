91ab950d01dcff140ef72a460c61f14d7565cfe7
Nikita Fedorov <fedorovni1996@yandex.ru>
2018-08-27 01:12:50 +0500

    made 1,3,4 task

---
+++ dz3Function/main.js
@@ -0,0 +1,52 @@
+/*------------------ЗАДАЧА №1 со встроенной переменной-----------------*/
+/*function multiply () {
+	let sum = 1;
+	for ( let i = 0; i < arguments.length; i++){
+		sum *= arguments[i];
+	}
+	return sum;
+}
+console.log(multiply(1,2,3));*/
+
+
+/*-------------------------ЗАДАЧА №1 с ... ---------------------------*/
+/*function multiply (...arg) {
+	let sum = 1;
+	for ( let i = 0; i < arg.length; i++){
+		sum *= arg[i];
+	}
+	return sum;
+}
+console.log(multiply(1,2,3));*/
+
+
+/*-------------------------ЗАДАЧА №2  ---------------------------*/
+
+
+
+/*-------------------------ЗАДАЧА №3  ---------------------------*/
+/*function reverseString (str) {
+	let i = str.length;
+	let st = "";
+	while (i--){
+		st += str[i];
+	}
+	return st;
+}
+
+console.log(reverseString('tset'));*/
+/*-------------------------ЗАДАЧА №4  ---------------------------*/
+/*function getCodeStringFromText (str) {
+	let st = "";
+	for ( let i = 0; i < str.length; i++){
+		st += str[i].charCodeAt() + " ";
+	}
+	return st;
+}
+console.log(getCodeStringFromText('hello'));*/
+
+/*-------------------------ЗАДАЧА №5  ---------------------------*/
+
+
+
+/*-------------------------ЗАДАЧА №6  ---------------------------*/
