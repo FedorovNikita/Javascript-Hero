618de1c3e678a6917e303262fb9e69312a244cc0
Nikita Fedorov <fedorovni1996@yandex.ru>
2018-09-04 22:13:43 +0500

    dz DOM

---
+++ dz4/dz-DOM.js
@@ -0,0 +1,20 @@
+// 1. Получить (в консоль head)
+/* let head = document.head;
+let head = document.querySelector('head'); */
+
+// 2 body
+/* let body = document.querySelector('body'); */
+/* let body = document.body;  */
+
+// 3 все дочерние элементы body
+/* let parentBody =  body.children; */
+
+// 4 первый div и все его дочерние узлы
+// a) все дочерние узлы в консоль
+/* let firstDiv = document.querySelector('div');
+let childDiv = firstDiv.children; */
+
+// б) в констоль все дочерние узлы, кроме первого и последнего
+/* let res = Array.prototype.slice.call( childDiv, 1, -1); */
+
+console.log(res);
