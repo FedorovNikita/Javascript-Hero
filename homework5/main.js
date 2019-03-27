9a0f15dcceed73808c72cf60b2641875ca0165c7
Nikita Fedorov <fedorovni1996@yandex.ru>
2019-03-27 20:43:32 +0500

    get hm5 tasks from 5 to 8

--- homework5/main.js
+++ homework5/main.js
@@ -44,12 +44,42 @@ function getTextFromUl() {
 //---------------------------ЗАДАЧА 4-----------------------------------
 // В параграфе заменить все дочерние текстовые узлы на "-twxt-"
 // (вложенные теги должны остаться).
-let p = document.querySelector('p');
-//
-for (let i = 0; i < p.childNodes.length; i++) {
-    if (p.childNodes[i].nodeType == 3) {
-        p.childNodes[i].data = "-twxt-";
+// let p = document.querySelector('p');
+// for (let i = 0; i < p.childNodes.length; i++) {
+//     if (p.childNodes[i].nodeType == 3) {
+//         p.childNodes[i].data = "-twxt-";
+//     }
+// }
+
+//---------------------------ЗАДАЧА 5----------------------------------
+// Найти в коде список ul и добавить класс "list"
+let listUl = document.querySelector('ul');
+listUl.classList.add('list');
+
+//---------------------------ЗАДАЧА 6----------------------------------
+// Найти в коде ссылку, находящуюся после списка ul, и добавить id='link'
+//listUl.nextElementSibling.nextElementSibling.id = 'link';
+
+let lastLink = document.getElementsByTagName('a');
+lastLink[lastLink.length - 1].id = 'link';
+
+//---------------------------ЗАДАЧА 7----------------------------------
+// Найти li через один (начиная с самого первого) установить класс "item"
+
+let allLi = document.getElementsByTagName('li');
+
+for (let i = 0; i < allLi.length; i++) {
+    if (i % 2 == 0) {
+        allLi[i].classList.add('item');
     }
+}
+
+//---------------------------ЗАДАЧА 8----------------------------------
+// на все ссылки в примере установить класс "custom-link"
+let allA = document.getElementsByTagName('a');
+
+for (let i = 0; i < allA.length; i++) {
+    allA[i].classList.add('custom-link');
 }
 
 
