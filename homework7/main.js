41e7169c3ebecbdcb47e01983dfc1698b58f1a89
Nikita Fedorov <fedorovni1996@yandex.ru>
2019-04-02 18:33:32 +0500

    get tasks 3-6

--- homework7/main.js
+++ homework7/main.js
@@ -25,3 +25,72 @@ const price = {
     }
 }
 
+//---------------------------ЗАДАЧА 3-------------------------------
+// Дан объект и функция.
+// Внесите в этот код такие изменения, чтобы можно было 
+// вызвать user.getName и получить 'Nikita'
+/* const user = {name: 'Nikita'},
+    getUserName = function() {
+        return this.name;
+};
+
+user.getName = getUserName; */
+
+//---------------------------ЗАДАЧА 4-------------------------------
+// Создать объект, у которого будет поле высота и метод "увеличить высоту
+// на один". Метод должен возвращать новую высоту
+let objHeight = {
+    height: 10,
+    getHeight: function() {
+        return this.height;
+    },
+    increaseHeight: function () {
+        this.height++;
+    }
+}
+
+//---------------------------ЗАДАЧА 5-------------------------------
+// Создать объект "вычислитель", у которого есть числовое свойство "значение"
+// и методы "удвоить", "прибавить один", "отнять один".
+// Методы можно вызывать через точку, образуя цепочку методов
+let numerator = {
+    value: 1,
+    double: function() {
+        this.value += this.value;
+        return this;
+    },
+    plusOne: function() {
+        this.value++;
+        return this;
+    },
+    minusOne: function() {
+        this.value--;
+        return this;
+    }
+}
+
+//---------------------------ЗАДАЧА 6-------------------------------
+// Разобрать и объяснить, что тут происходит
+const user = {name: 'Abraham'},
+    otherUser = {
+        name: 'John',
+        getName: function() {
+            return this.name;
+        }
+    }
+
+user.getName; // undefined
+user.getName = otherUser.getName;
+
+user.getName(); // 'Abraham'
+otherUser.getName(); // 'John'
+
+
+
+
+
+
+
+
+
+
