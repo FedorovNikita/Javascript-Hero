bc1223e9921cbc541709f9adadbfcb30b837dd3e
Nikita Fedorov <fedorovni1996@yandex.ru>
2019-04-08 17:31:38 +0500

    get hm8 tasks 2-5

--- homework8/main.js
+++ homework8/main.js
@@ -22,9 +22,11 @@ function theFirstNumberYear(year) {
 //---------------------------ЗАДАЧА 2-------------------------------
 // Написать функцию, которая принимает дату в виде строки "29.03.1985", 
 // а возвращает день в русском формате.
-/* function dayFromDate(dat) {
-    let datArr = dat.split('.');
-    let newYear = new Date(datArr[2], datArr[1], datArr[0]);
+function dayFromDate(dat) {
+    /* let datArr = dat.split('.');
+    let newYear = new Date(datArr[2], datArr[1] - 1, datArr[0]); */
+
+    let newYear = new Date(dat.split('.').reverse().join('.'));
     let number = newYear.getDay();
 
     switch(number) {
@@ -40,5 +42,45 @@ function theFirstNumberYear(year) {
     return number;
 }
 
-console.log(dayFromDate('08.04.2019'));
- */
+//console.log(dayFromDate('08.04.2019'));
+
+//---------------------------ЗАДАЧА 3-------------------------------
+// Узнать, сколько прошло полных недель с начала года до сегодняшней даты
+let lastDate = Date.now() -  new Date('2019.01.01');
+let weeks = parseInt(lastDate / 1000 / 60 / 60 / 24 / 7); 
+
+//console.log(`С начала года прошло ${weeks} нед.`);
+
+//---------------------------ЗАДАЧА 4-------------------------------
+// Напишите код, который будет выводить в консоль строку, начиная с
+// одной буквы и добавлять по одному символу каждые 200 мc
+/* let text = 'Nikita';
+let lengthText = text.length;
+let count = 0;
+let newText = '';
+
+setTimeout(function tex() {
+    if(lengthText--) {
+        newText += text[count++];
+        console.log(newText);
+        setTimeout(tex, 200);
+    } 
+}, 100) */
+
+//---------------------------ЗАДАЧА 5-------------------------------
+// Создайте функцию, которая принимает строку и время в секундах.
+// При вызове функции она должна показать переданный текст через заданное
+// количество секунд
+function getTextInTime(text, timer) {
+    setTimeout(() => console.log(text), timer);
+}
+
+getTextInTime('test', 1000);
+
+
+
+
+
+
+
+
