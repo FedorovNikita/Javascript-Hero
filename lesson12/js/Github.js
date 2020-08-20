7556c38a8039d13ec7ffa859b4c4b7b7c46ec452
Fedorov Nikita <fedorovni1996@yandex.ru>
2020-08-20 11:42:40 +0500

    added lesson12

---
+++ lesson12/js/Github.js
@@ -0,0 +1,28 @@
+class Github {
+  constructor() {
+    this.client_id = 'd3fa8b45af62166e6cf7';
+    this.client_secret = 'ff2d1c042c0bec254bcc8f7c965e0721fc7df37c'; 
+  }
+
+  // Get user by name
+  getUser(name) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://api.github.com/users/${name}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        .then(res => res.json())
+        .then(user => resolve(user))
+        .catch(err => reject(err))
+    })
+  }
+
+  // Get repos by name
+  getRepos(user) {
+    return new Promise((resolve, reject) => {
+      if (!user.login) reject('User nor found');
+
+      fetch(`https://api.github.com/users/${user.login}/repos?per_page=${5}&sort=${'created: asc'}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        .then(res => res.json())
+        .then(user => resolve(user))
+        .catch(err => reject(err))
+    })
+  }
+}
