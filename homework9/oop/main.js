05c8f2704a737b669e5367cde346b50a500be728
Nikita Fedorov <fedorovni1996@yandex.ru>
2021-09-21 19:36:12 +0500

    solved all tasks

--- homework9/oop/main.js
+++ homework9/oop/main.js
@@ -166,47 +166,246 @@
 // У класса "Гость" должно быть свойство "срокДействия" (validDate, например - поле также должно бытьс скрым), содержащее дату (например, одну неделю от момента регистрации)
 // У классов-наследников метод "получить информацию" должен также информацию о дополинтельый свойствах ("суперАдмин" и "срокДействия")
 
-function User(userName, dateRegistration) {
-    this.userName = userName;
-    this.dateRegistration = dateRegistration;
-}
+// function User(userName, dateRegistration) {
+//     this.userName = userName;
+//     this.dateRegistration = dateRegistration;
+// }
 
-User.prototype.getInfo = function() {
-    return {
-        userName: this.userName,
-        dateRegistration: this.dateRegistration,
+// User.prototype.getInfo = function() {
+//     return {
+//         userName: this.userName,
+//         dateRegistration: this.dateRegistration,
+//     }
+// }
+
+// function Admin(userName, dateRegistration, isSuperAdmin) {
+//     User.call(this, userName, dateRegistration);
+//     this._superAdmin = isSuperAdmin;
+// }
+
+// Admin.prototype = Object.create(User.prototype);
+// Admin.prototype.constructor = Admin;
+
+// Admin.prototype.getInfo = function() {
+//     const originMethod = User.prototype.getInfo.call(this);
+//     originMethod['isSuperAdmin'] = this._superAdmin;
+//     return originMethod;
+// }
+
+// function Guest(userName, dateRegistration) {
+//     User.call(this, userName, dateRegistration);
+//     this._validDate = new Date(+(new Date(this.dateRegistration)) + 604800000).toLocaleDateString();
+// }
+
+// Guest.prototype = Object.create(User.prototype);
+// Guest.prototype.constructor = Guest;
+
+// Guest.prototype.getInfo = function() {
+//     const originMethod = User.prototype.getInfo.call(this);
+//     originMethod['validDate'] = this._validDate;
+//     return originMethod;
+// }
+
+// let newUser = new User('Nik', '06.06.21');
+// let newAdmin = new Admin('Nik2', '07.07.21', true);
+// let newGuest = new Guest('Nik3', '08.08.21');
+
+
+// ====== Задания на функциональное наследование =======
+// =================== Задача 1 ===================
+
+// function General() {
+//     this.test = true;
+//     this.parent = true;
+//     this.getInfo = function () {
+//         return {test: this.test, parent: this.parent};
+//     }       
+// } 
+
+//   Создайте наследника от General, у которого будет parent = false и появится новоё свойство child = true. Метод getInfo должен вернуть объект как из оригинальной функции + поле child ({test: .., parent: …, child: ...})
+
+
+// function General() {
+//     this.test = true;
+//     this.parent = true;
+//     this.getInfo = function () {
+//         return {test: this.test, parent: this.parent};
+//     }       
+// } 
+
+// function ChildGeneral() {
+//     General.call(this);
+//     this.parent = false;
+//     this.child = true;
+//     let originalResult = this.getInfo();
+
+//     this.getInfo = function() {
+//         originalResult['child'] = this.child;
+//         return originalResult;
+//     }
+// }
+
+// let child = new ChildGeneral();
+
+// ============== Задача 2 ==========
+// Создайте класс Мальчик, который при инициализации принимает имя. 
+// У этого класса есть свойство “хороший”=true.
+// У класса так же есть метод “получить имя”, который должен вернуть строку вида:
+// “Мальчика зовут ” + имя + “. Он ” + <свойство хороший> === true ? “хороший” : “плохой”.
+// Далее создайте наследника от Мальчика - ПлохойМальчик. Установите свойство “хороший” равным false. Метод “получить имя” должен показывать алёрт со строкой из оригинального метода и добавочным текстом с новой строки “Не дружите с ним”.
+// Подсказка: для перевода на новую строку используйте символ \n
+
+// function Boy(name) {
+//     this.nameBoy = name;
+//     this.good = true;
+//     this.getName = function() {
+//         return `Мальчика зовут ${this.nameBoy}. Он ${this.good ? 'хороший' : 'плохой'}`;
+//     }
+// }
+
+// function BadBoy(name) {
+//     Boy.call(this, name);
+//     this.good = false;
+//     const originalResult = this.getName();
+
+//     this.getName = function() {
+//         return `${originalResult}. Не дружите с ним.`
+//     }
+// }
+
+// const tom = new Boy('Tom');
+// const mike = new BadBoy('Mike');
+// console.log(tom);
+// console.log(mike);
+
+// ================ Задача 3 ============
+// Создайте класс Fruit, который при инициализации принимает строковые аргументы “имя” и “цвет”. У класс есть один метод - “получить информацию”, который возвращает информацию о фрукте в виде строки “Фрукт” + имя + “цвет“ + цвет (“Фрукт - апельсин, цвет - оранжевый”).
+// Создайте наследника от Fruit - SweetFruit. У него появится свойство sweetness=true.  Метод “получить информацию” должен вернуть строку “Фрукт - апельсин, цвет - оранжевый, сладость - true”. Также добавить метод “установить сладость”, который позволит установить sweetness в любое значение (например, sweetness = “сладкий”). Метод “получить информацию” должен показывать актуальное значение sweetness (“Фрукт - апельсин, цвет - оранжевый, сладость - сладкий”)
+
+// function Fruit(name, color) {
+//     this.friutName = name;
+//     this.fruitColor = color;
+//     this.getInfo = function() {
+//         return `Фрукт - ${this.friutName}, цвет - ${this.fruitColor}`;
+//     }
+// }
+
+// function SweetFruit(name, color) {
+//     Fruit.call(this, name, color)
+//     this.sweetness = true;
+//     let originalResult = this.getInfo();
+
+//     this.setSweetness = function(sweet) {
+//         this.sweetness = sweet;
+//         return this;
+//     }
+
+//     this.getInfo = function() {
+//         return `${originalResult}, сладость - ${this.sweetness}`;
+//     }
+// }
+
+// const apple = new Fruit('apple', 'green');
+// const pineapple = new SweetFruit('pineapple', 'yellow'); 
+// console.log(apple);
+// console.log(pineapple);
+
+// Задания на прототипное наследование
+// ====================== Задача 1 ================
+
+// // Дан класс User. Создать наследника NewUser, у которого метод setNewName будет ко всему создавать новое свойство this.changed = “The field ‘name’ was changed!”.
+// function User(name, age) {
+//   this.name = name || 'Unknown';
+//   this.age = age || 0;
+// }
+
+// User.prototype.setNewName = function (name) {
+//   this.name = name;
+// };
+
+
+// function User(name, age) {
+//     this.name = name || 'Unknown';
+//     this.age = age || 0;
+// }
+
+// User.prototype.setNewName = function(name) {
+//     this.name = name;
+// };
+
+// function NewUser(name, age) {
+//     User.call(this, name, age);
+// }
+
+// NewUser.prototype = Object.create(User.prototype);
+// NewUser.prototype.constructor = NewUser;
+
+// NewUser.prototype.setNewName = function(name) {
+//     const oldName = this.name;
+//     const originMethod = User.prototype.setNewName(name);
+//     this.changed = `The field ${oldName} was changed on ${name}`;
+// }
+
+// const nik = new NewUser('Nik', 25);
+
+// ==================== Задача 2 =================
+// Дан абстрактный класс Module. 
+// Разобраться, что он делает и как работает.
+// // Создать наследника NewModule. Его setGlobalName должен делать всё то же самое, что и родительская функция, + устанавливать this.length = this.name.length.
+// function Module(name) {
+//   if (typeof name !== 'string') {
+//     throw 'Enter a name!';
+//   }
+  
+//   this.getName = function () {
+//     return name;
+//   };
+// }
+
+// Module.prototype.getPrettyName = function () {
+//   let name = this.getName();  
+//   name = name.replace(/\s+/, '_');  
+//   return 'The module name is ' + name[0].toUpperCase() + name.substr(1);
+// };
+
+// Module.prototype.setGlobalName = function (prefix) {
+//   let name = this.getName();
+//   this.name = prefix ? prefix + '_' + name : name; };
+
+
+
+function Module(name) {
+    if (typeof name !== 'string') {
+        throw 'Enter a name!';
     }
+    
+    this.getName = function () {
+        return name;
+    };
 }
 
-function Admin(userName, dateRegistration, isSuperAdmin) {
-    User.call(this, userName, dateRegistration);
-    this._superAdmin = isSuperAdmin;
+Module.prototype.getPrettyName = function () {
+    let name = this.getName();  
+    name = name.replace(/\s+/, '_');  
+    return 'The module name is ' + name[0].toUpperCase() + name.substr(1);
+};
+
+Module.prototype.setGlobalName = function (prefix) {
+    let name = this.getName();
+    this.name = prefix ? prefix + '_' + name : name;
+};
+
+function NewModule(name) {
+    Module.call(this, name);
 }
 
-Admin.prototype = Object.create(User.prototype);
-Admin.prototype.constructor = Admin;
+NewModule.prototype = Object.create(Module.prototype);
+NewModule.prototype.constructor = NewModule;
 
-Admin.prototype.getInfo = function() {
-    const originMethod = User.prototype.getInfo.call(this);
-    originMethod['isSuperAdmin'] = this._superAdmin;
-    return originMethod;
+NewModule.prototype.setGlobalName = function(prefix) {
+    Module.prototype.setGlobalName.call(this, prefix);
+    this.length = this.name.length;
 }
 
-function Guest(userName, dateRegistration) {
-    User.call(this, userName, dateRegistration);
-    this._validDate = new Date(+(new Date(this.dateRegistration)) + 604800000).toLocaleDateString();
-}
-
-Guest.prototype = Object.create(User.prototype);
-Guest.prototype.constructor = Guest;
-
-Guest.prototype.getInfo = function() {
-    const originMethod = User.prototype.getInfo.call(this);
-    originMethod['validDate'] = this._validDate;
-    return originMethod;
-}
-
-let newUser = new User('Nik', '06.06.21');
-let newAdmin = new Admin('Nik2', '07.07.21', true);
-let newGuest = new Guest('Nik3', '08.08.21');
+const mod = new NewModule('Nik');
 
